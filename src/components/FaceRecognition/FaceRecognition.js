84820bbd52f44e6eabc765ebf1e2badb522b2ae7
Leila <leila.imanova.7@gmail.com>
2023-04-07 18:51:05 +0400

    style changes

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,7 +6,7 @@ const FaceRecognition = ({imageUrl, box}) => {
     return(
         <div className="mt-4 w-full h-auto flex justify-center"> 
         <div className="absolute"> 
-        <img id="inputImg" src={imageUrl}  style={{maxWidth: "500px"}} /> 
+        <img id="inputImg"  src={imageUrl} className="detected_img"  style={{maxWidth: "500px"}} /> 
         <div className="bounding-box absolute flex flex-wrap justify-center shadow bg-transparent border-2 border-sky-500 cursor-pointer" 
         style={{ top: box.topRow, bottom: box.bottomRow, left: box.leftCol, right: box.rightCol}} >  </div>
         </div>
